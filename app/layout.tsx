16001efc3d482060312811c2959037e81729d32f
Milan <milanpavlovic02@icloud.com>
2025-08-15 13:36:41 +0200

    Add wave-hero SVG graphic for visual enhancement

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "@/components/ui/sonner";
+import wave from "@/public/wave-hero.svg";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ModeToggle } from "@/components/switch-theme";
+import { Inter } from "next/font/google";
+import Image from "next/image";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"], display: "swap" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={inter.className}>
       <head />
       <body>
         <ThemeProvider
@@ -35,8 +28,16 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <div className="fixed -top-10 h-30 w-full md:top-0 lg:h-20">
+            <Image
+              src={wave}
+              alt="Kanban Method"
+              fill
+              className="object-cover"
+            />
+          </div>
           <div className="px-4">
-            <div className="fixed top-2 left-4">
+            <div className="fixed top-2 right-4 z-20">
               <ModeToggle />
             </div>
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,51 @@
+import { Button } from "@/components/ui/button";
+import x from "@/public/kanban-method-animate.svg";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="relative mt-10 flex w-full flex-col-reverse items-center justify-start md:mt-0 md:flex-row md:justify-center">
+      <div className="flex w-full flex-col gap-4 text-center font-medium md:w-1/3 md:text-left">
+        <h1 className="text-primary text-5xl font-bold lg:text-7xl">
+          {/* Kanban title project */}
+          Welcome to Kanban Board
+        </h1>
+
+        <p className="text-muted-foreground mt-1 text-sm leading-relaxed text-balance">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima
+          repellendus veniam perferendis nihil, fugit esse et ipsum repellat ut
+          autem aliquam natus deleniti aliquid fuga, nobis provident, alias
+          nulla assumenda?
+        </p>
+        <div className="flex w-full items-center justify-center gap-4 md:justify-start">
+          <Button className="cursor-pointer" size="lg" asChild>
+            <Link href="/sign-in">
+              <span>Sign In</span>
+            </Link>
+          </Button>
+          <Button
+            variant="secondary"
+            className="cursor-pointer"
+            size="lg"
+            asChild
+          >
+            <Link href="/sign-up">
+              <span>Sign Up</span>
+            </Link>
+          </Button>
+        </div>
+      </div>
+
+      {/* Image */}
+      <div className="relative flex h-80 w-4/5 md:h-screen md:w-1/2">
+        <Image
+          src={x}
+          alt="Scrum Board"
+          fill
+          className="mask-radial-[100%_80%] mask-radial-from-75% mask-radial-at-top md:mask-radial-[100%_70%] md:mask-radial-at-right"
+        />
+      </div>
     </div>
   );
 }
